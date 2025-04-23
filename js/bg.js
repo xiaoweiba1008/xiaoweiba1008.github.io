246ccb247de44235c9603dca15d14ad85c071d3b
xiaoweiba1008 <xiaoweiba1008@163.com>
2025-04-24 04:23:43 +0800

    Site updated: 2025-04-24 04:23:43

--- js/bg.js
+++ js/bg.js
@@ -1,7 +1,18 @@
+document.querySelector("#scroll-down").style.zIndex="1"
 let header = document.querySelector("#page-header")
-let video = document.createElement("video")
-video.width="100%"
-video.loop = "loop"
-video.autoplay="autoplay"
-video.src="/bg.mp4"
-header.appendChild(video)
+header.style.overflow="hidden"
+// let video = document.createElement("video")
+// video.width="99%"
+// video.loop = "loop"
+// video.autoplay="autoplay"
+// video.src="/images/bg.mp4"
+// video.muted="muted"
+// header.appendChild(video)
+// video.play()
+
+
+// 使用模板字符串和insertAdjacentHTML构建并插入HTML
+const template = `
+<video width="100%" loop autoplay muted src="/images/bg.mp4"></video>
+`;
+header.insertAdjacentHTML('beforeend', template); // 'beforeend' 表示在body的末尾插入
